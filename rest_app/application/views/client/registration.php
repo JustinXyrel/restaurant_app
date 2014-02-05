7323c6e77436de704d68e63405fdf21fbfb65424
raphael <raphael_1220@yahoo.com>
2014-02-05 13:44:41 +0800

    .

--- rest_app/application/views/client/registration.php
+++ rest_app/application/views/client/registration.php
@@ -2,140 +2,24 @@
 <hr>
 
 <div class="alert alert-danger" id="error_message" style="display:none;"></div>
-<form id="registration" name="registration" method="post" action="">
-<table>
-	<tr>	
-		<td>
-			First Name
-		</td>
-		<td>
-			<input type="text" name="first_name" id="first_name">
-		</td>
-	</tr>
-	<tr>
-		<td>
-			Middle Name
-		</td>
-		<td>
-			<input type="text" name="middle_name" id="middle_name">
-		</td>
-	</tr>
-	<tr>
-		<td>
-			Last Name
-		</td>
-		<td>
-			<input type="text" name="last_name" id="last_name">
-		</td>
-	</tr>
-	<tr>
-		<td>
-			Gender
-		</td>
-		<td>
-			<input type="radio" name="gender" id="gender" value="male">Male</input>
-			<input type="radio" name="gender" id="gender" value="female">Female</input>
-		</td>
-	</tr>
-	<tr>
-		<td>
-			Unit no.
-		</td>
-		<td>
-			<input type="text" name="unit_no" id="unit_no">
-		</td>
-	</tr>
-	<tr>
-		<td>
-			Building no/Name
-		</td>
-		<td>
-			<input type="text" name="building_no" id="building_no">
-		</td>
-	</tr>
-	<tr>
-		<td>
-			Street
-		</td>
-		<td>
-			<input type="text" name="street" id="street">
-		</td>
-	</tr>
-	<tr>
-		<td>
-			City/Town
-		</td>
-		<td>
-			<input type="text" name="City/Town" id="city/town">
-		</td>
-	</tr>
-	<tr>
-		<td>
-			State/Province
-		</td>
-		<td>
-			<input type="text" name="city/town" id="state/province">
-		</td>
-	</tr>
-	<tr>
-		<td>
-			Country
-		</td>
-		<td>
-			<input type="text" name="country" id="country">
-		</td>
-	</tr>
-	<tr>
-		<td>
-			Date of Birth
-		</td>
-		<td>
-			<input type="text" name="dob" id="dob">
-		</td>
-	</tr>
-	<tr>
-		<td>
-			<br/>
-			<h2>Contact Information</h2>
-			<hr>
-		</td>
-	</tr>
-	<tr>	
-		<td>
-			Contact No.
-		</td>
-		<td>
-			<input type="text" name="contact_no" id="contact_no">
-		</td>
-	</tr>
-	<tr>	
-		<td>
-			Email Address
-		</td>
-		<td>
-			<input type="text" name="email" id="email">
-		</td>
-	</tr>
-	<tr>	
-		<td>
-			Username
-		</td>
-		<td>
-			<input type="text" name="username" id="username">
-		</td>
-	</tr>
-	<tr>	
-		<td>
-			Password
-		</td>
-		<td>
-			<input type="text" name="password" id="password">
-		</td>
-	</tr>
-	<tr>
-		<td>
-			<input type="submit" value="Submit">
-		</td>
-	</tr>
-</table>
+<form id="registration" name="registration" method="post" action="" class="form-horizontal" role="form">
+	<div class="form-group" id="div_first_name">
+		<label for="first_name" class="col-sm-2 control-label">First Name</label>
+		<div class="col-sm-5">
+			<input type="text" class="form-control" id="first_name" placeholder="First Name" name="first_name">
+		</div>
+	</div>
+	<div class="form-group" id="div_middle_name">
+		<label for="middle_name" class="col-sm-2 control-label">Middle Name</label>
+		<div class="col-sm-5">
+			<input type="text" class="form-control" id="middle_name" placeholder="Middle Name" name="middle_name">
+		</div>
+	</div>
+	
+	<div class="form-group">
+		<div class="col-sm-offset-2 col-sm-5">
+			<button type="submit" class="btn btn-default">Sign in</button>
+		</div>
+	</div>
+
 </form>
--- rest_app/includes/JS/functions.js
+++ rest_app/includes/JS/functions.js
@@ -6,10 +6,18 @@ $('#registration').ajaxForm({
 	beforeSubmit: function(){
 		if($("#first_name").val() == ""){
 			$("#error_message").html("First Name is required");
+			$("#div_first_name").addClass("has-error");
+			$("#error_message").show("slow");
+			return false;
+		}
+		if($("#middle_name").val() == ""){
+			$("#div_first_name").removeClass("has-error");
+			$("#error_message").html("Middle Name is required");
+			$("#div_middle_name").addClass("has-error");
 			$("#error_message").show("slow");
 		}
 	},
 	success: function(data){
 
-		}
+	}
 });
