7a26876fb874a80ea84a64cdf65651c55b45ef61
raphael <raphael_1220@yahoo.com>
2014-02-05 13:13:02 +0800

    .

--- rest_app/application/views/client/registration.php
+++ rest_app/application/views/client/registration.php
@@ -1,10 +1,137 @@
+<h2>Personal Information</h2>
+<hr>
 <table>
 	<tr>	
 		<td>
-			First name
+			First Name
 		</td>
 		<td>
 			<input type="text" name="first_name" id="first_name">
 		</td>
+	</tr>
 	<tr>
+		<td>
+			Middle Name
+		</td>
+		<td>
+			<input type="text" name="middle_name" id="middle_name">
+		</td>
+	</tr>
+	<tr>
+		<td>
+			Last Name
+		</td>
+		<td>
+			<input type="text" name="last_name" id="last_name">
+		</td>
+	</tr>
+	<tr>
+		<td>
+			Gender
+		</td>
+		<td>
+			<input type="radio" name="gender" id="gender" value="male">Male</input>
+			<input type="radio" name="gender" id="gender" value="female">Female</input>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			Unit no.
+		</td>
+		<td>
+			<input type="text" name="unit_no" id="unit_no">
+		</td>
+	</tr>
+	<tr>
+		<td>
+			Building no/Name
+		</td>
+		<td>
+			<input type="text" name="building_no" id="building_no">
+		</td>
+	</tr>
+	<tr>
+		<td>
+			Street
+		</td>
+		<td>
+			<input type="text" name="street" id="street">
+		</td>
+	</tr>
+	<tr>
+		<td>
+			City/Town
+		</td>
+		<td>
+			<input type="text" name="City/Town" id="city/town">
+		</td>
+	</tr>
+	<tr>
+		<td>
+			State/Province
+		</td>
+		<td>
+			<input type="text" name="city/town" id="state/province">
+		</td>
+	</tr>
+	<tr>
+		<td>
+			Country
+		</td>
+		<td>
+			<input type="text" name="country" id="country">
+		</td>
+	</tr>
+	<tr>
+		<td>
+			Date of Birth
+		</td>
+		<td>
+			<input type="text" name="dob" id="dob">
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<br/>
+			<h2>Contact Information</h2>
+			<hr>
+		</td>
+	</tr>
+	<tr>	
+		<td>
+			Contact No.
+		</td>
+		<td>
+			<input type="text" name="contact_no" id="contact_no">
+		</td>
+	</tr>
+	<tr>	
+		<td>
+			Email Address
+		</td>
+		<td>
+			<input type="text" name="email" id="email">
+		</td>
+	</tr>
+	<tr>	
+		<td>
+			Username
+		</td>
+		<td>
+			<input type="text" name="username" id="username">
+		</td>
+	</tr>
+	<tr>	
+		<td>
+			Password
+		</td>
+		<td>
+			<input type="text" name="password" id="password">
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<input type="submit" value="Submit">
+		</td>
+	</tr>
 </table>
